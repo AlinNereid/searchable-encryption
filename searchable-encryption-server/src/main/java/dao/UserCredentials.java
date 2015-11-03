4b734c8b818dce61cd5c34f8a9b0c1024f28346c
Alexandru Buzneanu <bznnalex@gmail.com>
2015-11-03 20:37:48 +0200

    Added document and user credentials dao

---
+++ searchable-encryption-server/src/main/java/dao/UserCredentials.java
@@ -0,0 +1,18 @@
+package dao;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+/**
+ * Created by Alexandru on 11/2/2015.
+ */
+@Entity
+public class UserCredentials implements Serializable {
+
+    private String username;
+    private Long userId;
+    private boolean isValid;
+    private GrantedAuthority userAuthority;
+}
